fbf5ad3cc62b21ff1dab0cf4ecac4fe25ab4dc42
notyourgiantcoder <154630963+notyourgiantcoder@users.noreply.github.com>
2023-12-23 02:21:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "CHAL KISSI KAR";
+  gif.src =
+    "https://media2.giphy.com/media/frHK797nhEUow/giphy.gif?cid=ecf05e478cj0f7kgfdytuonggp8im7hzed9z299qpfs4ig0o&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+}
+);
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
